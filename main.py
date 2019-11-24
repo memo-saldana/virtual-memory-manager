400135748eb7231b97d3e8c6ec6c6a9f5ff166a9
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-11-23 18:59:12 -0600

    Adds strategy boolean

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import sys
 from instruction_parser import parse_instructions
 
+""" 
+	Bool that sets strategy for simulation
+	true - uses fifo
+	false - uses lru		 
+"""
+strategy
 
 def main():
 	if(len(sys.argv) < 2 or sys.argv[1] not in ['fifo', 'lru']):
@@ -8,7 +14,10 @@ def main():
 		exit()
 	print('script with name ', sys.argv[0])
 	print('replacement strategy ', sys.argv[1])
+	strategy = True if argv[1] == 'fifo' else False
+
 	instructions = parse_instructions()
 	for instruction in instructions:
 		print(instruction)
+		# Add calls to each instruction
 main()
